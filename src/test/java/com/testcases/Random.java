027a3e4c718744116d9d66ac566825ee8a2918f2
nikhilsharma026 <nikhilsharma026@gmail.com>
2019-12-01 19:24:58 +0530

    Firs commit

---
+++ src/test/java/com/testcases/Random.java
@@ -0,0 +1,16 @@
+package com.testcases;
+
+public class Random {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=0,j=5;
+		//j=i++;
+		j=i++;
+		j=i++;
+		System.out.println(j);
+		
+	}
+
+}
